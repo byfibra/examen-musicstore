4350294a9b251ad51c54f92a4256cf7fb24ffc81
byfibra <angel.lp.318@gmail.com>
2026-05-15 09:50:55 +0200

    Ejercicio 2: lista, búsqueda y fichero

--- ExamenEntornos3EvalDAMA/Program.cs
+++ ExamenEntornos3EvalDAMA/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExamenEntornos3EvalDAMA;
+
+List<Album> lista = new List<Album>
+{
+    new Album("Master of Puppets", "Metallica", 1986, true),
+    new Album("Thriller", "Michael Jackson", 1982, false),
+    new Album("72 Seasons", "Metallica", 2023, true)
+};
+
+Console.WriteLine("TODOS LOS ALBUMES:");
+foreach (var a in lista) Console.WriteLine(a.ToString());
+
+Console.WriteLine("\nSOLO METALLICA:");
+foreach (var a in lista)
+{
+    if (a.GetArtista().Contains("Metallica")) Console.WriteLine(a.ToString());
+}
+
+Console.WriteLine("\nFecha actual: " + DateTime.Now.ToShortDateString());
+
+GuardarAlbums(lista, "albums.txt");
+
+static void GuardarAlbums(List<Album> lista, string ruta)
+{
+    using (StreamWriter sw = new StreamWriter(ruta))
+    {
+        foreach (var a in lista)
+        {
+            sw.WriteLine($"{a.GetTitulo()};{a.GetArtista()};{a.GetAnyo()};{a.IsDisponible()}");
+        }
+    }
+}
